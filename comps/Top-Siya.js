4b28c27f920a197d79b461ce6a25708c4f544c68
siyayang0420 <siyayang0420@live.com>
2019-10-15 20:58:18 -0700

    finished 123

---
+++ comps/Top-Siya.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet,Switch, Alert} from 'react-native';
+
+function TopSiya(){
+    const [changeSwitch, setChangeSwitch] = useState(false);
+
+    return(
+        <View style={[styles.container, {backgroundColor: changeSwitch? 'black' : 'white'} ]}>
+            <Text style={{color: changeSwitch? '#FFFFFF' : 'black'}}>{changeSwitch? 'Dark Mode' : 'Day Mode'}</Text>
+            <View>
+            <Switch
+                onValueChange={ () => {setChangeSwitch(!changeSwitch)}} 
+                value={changeSwitch} 
+            >
+            </Switch>
+            </View>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:'center',
+        alignItems:'center',
+        height:300,
+    }
+})
+
+export default TopSiya;
